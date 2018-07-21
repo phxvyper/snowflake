fa6df70c626744a8d611d2869daccb2c3a586f16
Tristen Horton <tristen@tristenhorton.com>
2018-07-21 00:46:43 -0700

    A simple flask server written in python 3.6 for testing snowflakes.

--- example/README.md
+++ example/README.md
@@ -1,21 +1,4 @@
 Example
 =======
 
-This example runs Sonyflake on AWS Elastic Beanstalk.
-
-Setup
------
-
-1. Build the cross compiler for linux/amd64 if using other platforms.
-
-  ```
-  cd $GOROOT/src && GOOS=linux GOARCH=amd64 ./make.bash
-  ```
-
-2. Build sonyflake_server in the example directory.
-
-  ```
-  ./linux64_build.sh
-  ```
-
-3. Upload the example directory to AWS Elastic Beanstalk.
+A simple flask server written in python 3.6 for testing snowflakes.
---
+++ example/snowflake_server.py
@@ -0,0 +1,36 @@
+from flask import Flask, Response
+from ctypes import windll, CDLL, byref, c_ulonglong
+import os
+
+APP = Flask(__name__)
+
+snowflakes = []
+workerid = 0
+
+if os.name == "nt":
+    snowflake_native = windll.LoadLibrary("./snowflake.so")
+else:
+    snowflake_native = CDLL("./snowflake.so")
+
+
+def next_snowflake():
+    snowflake = c_ulonglong()
+    success = snowflake_native.next_snowflake(byref(snowflake))
+
+    return snowflake.value if success == 0 else None
+
+
+@APP.route("/snowflake", methods=["POST"])
+def post_snowflake():
+    snowflake = next_snowflake()
+    snowflakes.append(snowflake)
+
+    return Response(str(snowflake), status=200)
+
+
+@APP.route("/snowflake/<string:snowflake>", methods=["GET"])
+def snowflake_verify(snowflake):
+    if snowflake in snowflakes:
+        return Response("OK", status=200)
+    else:
+        return Response("Not Found", status=404)
